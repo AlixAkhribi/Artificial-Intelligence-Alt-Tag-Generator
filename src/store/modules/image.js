b4c1b92bde148dbe08dbcee6df5742a611151dd9
Unknown <AlixAkhribi@yahoo.com>
2019-02-20 21:11:15 -0800

    Create initial store image module

---
+++ src/store/modules/image.js
@@ -0,0 +1,26 @@
+import api from '../../api/computerVision';
+
+const state = {
+    imageDescription: "",
+    imageTags: [],
+    imageURL: "",
+};
+
+const getters = {
+
+};
+
+const actions = {
+ 
+};
+
+const mutations = {
+
+};
+
+export default {
+    state,
+    getters,
+    actions,
+    mutations
+};
