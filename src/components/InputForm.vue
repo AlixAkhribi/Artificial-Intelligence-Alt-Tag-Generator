b7627c1e12f99298cfaae6a32dd81ac8b37d725d
Unknown <AlixAkhribi@yahoo.com>
2019-02-21 10:39:51 -0800

    Create InputForm component
    InputForm component to take user input and make API calls

---
+++ src/components/InputForm.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="image-input-form">
+    <form>
+      <p class="image-input-text">Image URL</p>
+      <input @change="caputreImageURL" placeholder="paste URL here">
+      <button @click="sendImageURL">Start Magic</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+
+export default {
+  name: "InputForm",
+  methods: mapActions(["caputreImageURL", "sendImageURL"]),
+};
+</script>
+
+<style lang="scss" scoped>
+.image-input-form {
+  font-size: 1.6rem;
+}
+</style>
