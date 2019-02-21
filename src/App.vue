80801faeaa56ad37b39da95e3bb12295d629c463
Unknown <AlixAkhribi@yahoo.com>
2019-02-20 21:09:27 -0800

    Create App component

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <ImageResults/>
+    <InputForm/>
+  </div>
+</template>
+
+<script>
+import ImageResults from "./components/ImageResults";
+import InputForm from "./components/InputForm";
+
+export default {
+  name: "app",
+  components: {
+    ImageResults,
+    InputForm
+  }
+};
+
+</script>
+
+<style lang="scss">
+:root {
+  font-size: 62.5%;
+}
+</style>
+
