e8f1624907afb1fc046a7452f27a6e901384c5b3
Unknown <AlixAkhribi@yahoo.com>
2019-02-22 23:03:50 -0800

    Change default state imageTags to null, create async sendImageURL

--- src/store/modules/image.js
+++ src/store/modules/image.js
@@ -2,22 +2,33 @@ import api from '../../api/computerVision';
 
 const state = {
     imageDescription: "",
-    imageTags: [],
+    imageTags: null,
     imageURL: "",
 };
 
 const getters = {
-    allImageData: (state) => ({...state}),
+    allImageData: (state) => ({ ...state }),
 };
 
 const actions = {
-    caputreImageURL: ({commit}, event) => {
+    caputreImageURL: ({ commit }, event) => {
         commit('setImageURL', event.target.value)
+    },
+
+    // Function takes the URL from state and makes POST request to API
+    async sendImageURL({ rootState, commit }, event) {
+        event.preventDefault();
+
+        const { imageURL } = rootState.image;
+        let response = await api.sendImageToAnalyze(imageURL);
+        
+        commit('setImageData', response);
     }
 };
 
 const mutations = {
-    setImageURL: (state, url) => state.imageURL = url
+    setImageURL: (state, url) => state.imageURL = url,
+
 };
 
 export default {
