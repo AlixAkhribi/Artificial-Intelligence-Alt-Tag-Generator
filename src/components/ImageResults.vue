0b7de5ad75f06b061cbf6a691496d2d0510884f1
Unknown <AlixAkhribi@yahoo.com>
2019-02-22 23:08:15 -0800

    Create ImageResults component
    Component that will display all final results of the analyzed image

---
+++ src/components/ImageResults.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="image-results card">
+    <h1 class="header">Image Response</h1>
+    <div class="image-results-wrapper">
+      <div class="image-results-title"></div>
+      <div class="image-results-photo">
+        <img :src="allImageData.imageURL" :alt="allImageData.imageDescription">
+      </div>
+      <div class="image-results-description">
+        <p>{{allImageData.imageDescription}}</p>
+      </div>
+      <div class="image-results-image">
+        <img src alt>
+      </div>
+      <div class="image-results-description"></div>
+      <div class="image-results-tags">
+        <p>{{allImageData.imageTags}}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "Image-Results",
+  computed: mapGetters(["allImageData"])
+};
+</script>
+
+<style lang="scss" scoped>
+.image-results {
+  font-size: 1.6rem;
+  height: 60rem;
+  width: 50%;
+
+  .header {
+    display: flex;
+    justify-content: center;
+  }
+}
+
+.image-results-wrapper {
+  padding: 5px 15px;
+  margin: 5px 20px;
+}
+
+.image-results-photo {
+  text-align: center;
+
+  img {
+    height: 35rem;
+  }
+}
+
+.image-results-description{
+  text-align: center
+}
+</style>
