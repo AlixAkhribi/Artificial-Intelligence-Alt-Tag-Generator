b2038e73a3849f7d7e1aaf0d66dcd90b888467b5
Unknown <AlixAkhribi@yahoo.com>
2019-02-24 19:51:21 -0800

    ImageResults component styling

--- src/components/ImageResults.vue
+++ src/components/ImageResults.vue
@@ -1,20 +1,17 @@
 <template>
   <div class="image-results card">
-    <h1 class="header">Image Response</h1>
+    <h1 class="card-header">Image Response</h1>
     <div class="image-results-wrapper">
-      <div class="image-results-title"></div>
       <div class="image-results-photo">
         <img :src="allImageData.imageURL" :alt="allImageData.imageDescription">
       </div>
-      <div class="image-results-description">
-        <p>{{allImageData.imageDescription}}</p>
-      </div>
-      <div class="image-results-image">
-        <img src alt>
-      </div>
-      <div class="image-results-description"></div>
-      <div class="image-results-tags">
-        <p>{{allImageData.imageTags}}</p>
+      <div class="image-results-tabs-wrapper">
+        <ul class="tabs-title">
+          <li name="image-results-description" class="tab-title">Description</li>
+        </ul>
+        <div class="image-results-tags tab-child">
+          <p>{{allImageData.imageDescription}}</p>
+        </div>
       </div>
     </div>
   </div>
@@ -35,27 +32,44 @@ export default {
   font-size: 1.6rem;
   height: 60rem;
   width: 50%;
-
-  .header {
-    display: flex;
-    justify-content: center;
-  }
 }
 
 .image-results-wrapper {
+  position: absolute;
+  display: flex;
+  width: 90%;
+  flex-direction: column;
   padding: 5px 15px;
   margin: 5px 20px;
+  height: 85%;
+  justify-content: space-evenly;
 }
 
 .image-results-photo {
   text-align: center;
 
   img {
-    height: 35rem;
+    height: 37rem;
   }
 }
 
-.image-results-description{
-  text-align: center
+.image-results-tabs-wrapper {
+  text-align: center;
+  font-size: 1.6rem;
+  background: #4b4471;
+  box-shadow: 0.1rem 0.2rem 1.4rem black;
+}
+
+.image-results-description {
+  text-align: center;
+}
+
+.image-results-tabs {
+  display: flex;
+}
+
+.tab-title {
+  list-style: none;
+  padding: 0;
 }
 </style>
