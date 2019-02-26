4131600d2ccaa45ca775bca39694a0c1ffe9f0d1
Unknown <AlixAkhribi@yahoo.com>
2019-02-25 19:37:26 -0800

    Import Materialize into project

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.css';
+import 'vue-material/dist/theme/default-dark.css' 
 
 import store from './store';
 
-Vue.config.productionTip = false
+Vue.use(VueMaterial);
+Vue.config.productionTip = false;
 
 new Vue({
   store,
